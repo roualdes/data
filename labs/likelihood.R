16a3822594cf4b1f4796fccb39ced192139765e7
Edward A. Roualdes <edward.roualdes@uky.edu>
2016-02-23 11:57:47 -0800

    add likelihood material

---
+++ labs/likelihood.R
@@ -0,0 +1,15 @@
+## 1. Randomly generate a reasonable number of observations from the gamma distribution.
+## You must pick two parameters: shape and rate.
+## ?rgamma
+
+## 2. Make a histogram of your randomly generated gamma variables.
+## Describe the sample distribution using keywords from this class.
+## ?qplot
+
+## 3. Write the likelihood function that we will maximize.
+## l(theta|x_1, ..., x_n)
+
+## 4. Maximize your function above using R's optimizer funciton optim.
+## ?optim
+## The method "L-BFGS-B" is one of the more recent and more stable algorithms.
+
