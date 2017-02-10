f24798e099c1c413aceece257abc5997159afb75
Edward A. Roualdes <edward.roualdes@gmail.com>
2017-02-10 12:10:25 -0800

    add random variables lab

---
+++ labs/randomvars.R
@@ -0,0 +1,56 @@
+## 1. Consider the following discrete random variable, X.
+## x            | 6     7       8       9       10      11      12
+## P(X = x)     | 0.1   0.2     0.3     0.2     0.1     0.05     0.05
+
+## 1.a.
+## Calculate the expected value and variance; efficiency counts.
+
+## 1.b.
+## Randomly sample from the random variable X,
+## with samples sizes n = \{10, 100, 1000\}.
+## Hint: ?sample
+
+## 1.c.
+## Calculate the sample mean and sample variance for each random
+## sample/size combination.
+## Comment on the relative correctness of your three guesses.
+
+## 2. Assume the length of adult male Calponia harrisonfordis is
+## normally distributed with mean 18.14 mm and
+## standard deviation 1.76 mm. Hint: ?pnorm
+
+## 2.a.
+## What is the probability we would find a C. harrisonfordi that is
+## shorter than 14.66 mm or longer than 21.66 mm?
+
+## 2.b.
+## What is the length of the longest 7.5% of C. harrisonfordis?
+
+## 3. Randomly generate 1001 observations from a normal
+## distribution of your choice (pick mean and standard deviation).
+## Make a histogram from these observations. Hint: ?rnorm
+
+## 4. Randomly generate 1001 observations from a Bernoulli
+## distribution and estimate the variance using only the sample mean.
+## Hint: ?rbinom, size=1 and you pick `prob`.
+
+## 5. An airline charges the following baggage fees: $25 for the first
+## bag and $35 for the second. Suppose 54% of passengers have no
+## checked luggage, 34% have one piece of checked luggage and 12% have
+## two pieces. We suppose a negligible portion of people check more
+## than two bags.
+
+## 5.a.
+## What is the probability mass function for baggage fees?
+
+## 5.b.
+## Compute the average revenue per passenger, and compute the
+## corresponding variance.
+
+## 5.c.
+## About how much revenue should the airline expect for a flight of
+## 120 passengers? With what variance?
+
+## 5.d.
+##The current exchange rate for one U.S. dollar ($) to one Euro is
+## .9355.  What is the expected revenue and variance in Euros?
