2e2e1bea9e822c7de140436213f757a5b3f7adef
Edward A. Roualdes <edward.roualdes@gmail.com>
2017-04-28 13:42:26 -0700

    add regression_likelihood lab

---
+++ labs/regression_likelihood.R
@@ -0,0 +1,26 @@
+## Consider Darwin's finch data https://roualdes.us/data/finches.csv
+
+## Choose two numerical variables of interest, pick one to be the
+## explanatory, and plot the data.
+
+## Fit simple linear regression using lm().
+
+## Set up and conclude hypothesis tests for the slope and the intercept.
+
+## Calculate approriately matching confidence intervals.  Do your
+## confidence intervals agree with your hypothesis tests above?
+
+## Fit simple linear regression using optim().  Your estimates of the
+## slope and the intercept should match your work above.
+
+## Interpret the slope and intercept in context.
+
+## Bootstrap the standard errors of regression model using optim(),
+## using boot(..., method="bca").  Ensure your estimated standard
+## errors are close to the results of lm().
+
+## Make bootstrap confidence intervals of your estimated
+## coefficients. Note, these confidence intervals should be relatively
+## close, but not exact to your work above.
+
+## Interpret your confidence intervals in context.
